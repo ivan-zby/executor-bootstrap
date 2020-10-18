32abec6ac36911cd57cd034db2cc7931b32e0893
zhaobiyang <zhaobiyang@sina.com>
2020-10-18 20:26:34 +0800

    initial commit

---
+++ test/executor_test.cc
@@ -0,0 +1,22 @@
+#include <cctest/cctest.h>
+#include "executor/executor.h"
+#include "executor_id.h"
+#include "mcl/log.h"
+
+FIXTURE(ExecutorTest) {
+	BEFORE {
+		MCL_INFO("TEST SETUP");
+	}
+
+	AFTER{
+		MCL_INFO("TEST TEARDOWN");
+	}
+
+	TEST("should get executor name") {
+		MCL_DBG("TEST RUNING...");
+		ASSERT_EQ(0, executor_get_id());
+		MCL_SUCC("TEST SUCCESS!");
+	}
+
+
+};
